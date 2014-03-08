49d7e2ff04cbfcf9f5f84d998e592cb1c4e4ba52
blaze-x77 <blaze.x12@hotmail.com>
2014-03-08 08:27:40 -0500

    Update JavaApplication2.java

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -16,6 +16,29 @@ public class JavaApplication2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+              
+         int vector[]= new int[5];
+int Numero; //1
+for(int i=0; i < vector.length;i++){ //2
+  Numero=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el numero"));//3
+    if(Numero>8){//4
+     vector[i]= Numero;//5
+    }else{//6
+        JOptionPane.showInputDialog("No debe ingresar numeros menores que 8");//7
+        
+      }//8
     }
-    ///54646465r4e65f165r
+
+for(int i=0;i < vector.length;i++){//9
+    System.out.println("El numero ingresado en el vector fue:"+vector[i]);//10
+}
+System.exit(0);//10
+        
+        
+        
+        
+        
+        
+    }
+    
